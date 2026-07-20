f6fc1f1bda580fc8fc6c4f208cfeda9086bdd668
abadullah <abd.codes1@gmail.com>
2026-07-20 20:45:36 +0300

    Add files via upload

---
+++ task6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class task6 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        long dhoom = 99999, dhoom2 = 2;
+        int raid = 100, nawaf = 506;
+
+        int x = 1, y = 1;
+
+        double dhoom1 = 22.88 , dhoom3 = 22.87;
+
+    }
+}
