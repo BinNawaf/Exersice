7eed0205023357b76bb45ecfa83ee440cce36360
Abdullah <abd.codes1@gmail.com>
2026-08-12 17:05:38 +0300

    Add files via upload

---
+++ task18.java
@@ -0,0 +1,15 @@
+
+
+import java.util.*;
+
+public class task18 {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println(" Enter your name : ");
+        String name = input.nextLine();
+
+        System.out.println(  " halo big boss " + name );
+    }
+}
