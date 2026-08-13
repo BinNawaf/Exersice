67e742766fbc926e09724fcb2974c7da4751e61d
Abdullah <abd.codes1@gmail.com>
2026-08-13 12:36:27 +0300

    Add files via upload

---
+++ task19.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class task19 {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+        System.out.println(" Enter an ASCII code (0-127) : ");
+        int ascii = input.nextInt();
+
+        char abood = (char)ascii;
+
+        System.out.println(" The character is : " + abood);
+    }
+        
+    }
+
+    
+
