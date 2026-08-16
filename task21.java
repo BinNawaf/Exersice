4560436fd770ee8e46b111f8dafe837fa9b89542
Abdullah <abd.codes1@gmail.com>
2026-08-16 18:06:59 +0300

    Add files via upload

---
+++ task21.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class task21 {
+
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.println(" Enter tow numbers :  ");
+
+        double number1 = input.nextDouble();
+        double number2 = input.nextDouble();
+
+        double sum = number1 + number2;
+
+        double Difference = number1 - number2;
+
+        double Product = number1 * number2; 
+
+        double quotient = number1 / number2;
+
+
+        System.out.println( " Sum =  "  + sum + ("\n Difference = ") + Difference + ("\n Product = ") + Product + ("\n Quotient = ") + quotient  );
+
+
+
+
+
+
+
+    }
+
+}
